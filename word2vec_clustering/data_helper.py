889493efb1a0b1117b3c5cfb06392b8b92c3b624
roomylee <roomylee@naver.com>
2017-10-24 21:14:38 +0900

    separate data processor

---
+++ word2vec_clustering/data_helper.py
@@ -0,0 +1,71 @@
+import mysql.connector as msc
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+config = {'host': '***REMOVED***',
+              'port': '***REMOVED***',
+              'user': '***REMOVED***',
+              'password': '***REMOVED***',
+              'database': '***REMOVED***'}
+
+conn = msc.connect(**config)
+qry = conn.cursor(buffered=True)
+
+
+def get_triplet():
+    query = "SELECT Subject, Object, Relation FROM ***REMOVED***.relation"
+
+    qry.execute(query)
+    row = qry.fetchone()
+
+    bacteria_list = list()
+    disease_list = list()
+    relation_list = list()
+    while row is not None:
+        try:
+            bacteria_list.append(row[0])
+            disease_list.append(row[1])
+            relation_list.append(row[2])
+        except Exception as e:
+            print(e)
+            continue
+        row = qry.fetchone()
+
+    return bacteria_list, disease_list, relation_list
+
+def get_corpus(corpus_type='full'):
+    corpus_dic = {
+            # Full Corpus
+            'full': "SELECT sentence FROM bio_corpus",
+            # Relation-Contained Corpus
+            'relation': 'SELECT SENTENCE_STRING FROM result',
+            # Entity-Relation Triplets
+            'triplet': 'SELECT Subject, Object, Relation FROM relation'
+    }
+    corpus = []
+    lemmatizer = WordNetLemmatizer()
+
+    query = corpus_dic[corpus_type]
+    qry.execute(query)
+    row = qry.fetchone()
+    while row is not None:
+        try:
+            corpus.append([lemmatizer.lemmatize(w, 'v') for w in word_tokenize(' '.join(row))])
+        except Exception as e:
+            print(e)
+            continue
+        row = qry.fetchone()
+
+    return corpus
+
+if __name__ == "__main__":
+    # get_triplet() test
+    bac, dis, rel = get_triplet()
+    print("Bacteria :", bac[:5])
+    print("Disease :", bac[:5])
+    print("Relation :", bac[:5])
+
+    # get_corpus() test
+    corpus = get_corpus()
+    print(corpus[0])
